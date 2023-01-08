edea5852b3dd84b2b979709474652ee3ccea84f4
romaindesson <romain.desson@soundcast.fm>
2023-01-08 17:08:56 +0100

    Add new empty cols in export

--- src/utils/parseDataToCSV.tsx
+++ src/utils/parseDataToCSV.tsx
@@ -2,9 +2,9 @@ import { EbayObjectType } from "../component/home";
 
 export const parseDataToCSV = (data: EbayObjectType[]) => {
     const CSVArray = []
-    CSVArray.push(['Titre', 'Prix', 'Référence', 'Condition', 'Image', 'quantity', 'URL'])
+    CSVArray.push(['Numéro article', 'Catégorie', 'Titre', 'Référence', 'Condition', 'Quantité', 'Prix de vente', "Prix d'achat", 'Image', 'URL'])
     data.forEach((datum) => {
-        CSVArray.push([datum.title, datum.price, datum.ref, datum.condition, datum.image, datum.quantity, datum.url])
+        CSVArray.push([null, null, datum.title, datum.ref, datum.condition, datum.quantity, datum.price, null, datum.image, datum.url])
     })
     return CSVArray
 }
