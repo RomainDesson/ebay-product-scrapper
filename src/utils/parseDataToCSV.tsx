188bfc38a65a035f20f19facba54502da1538ae9
romaindesson <romain.desson.02320@gmail.com>
2024-05-24 13:26:52 +0200

    Add new columns to CSV

--- src/utils/parseDataToCSV.tsx
+++ src/utils/parseDataToCSV.tsx
@@ -2,9 +2,9 @@ import { EbayObjectType } from "../component/home";
 
 export const parseDataToCSV = (data: EbayObjectType[]) => {
     const CSVArray = []
-    CSVArray.push(['Numéro article', 'Catégorie', 'Titre', 'Référence', 'Condition', 'Quantité', 'Prix de vente', "Prix d'achat", 'Image', 'URL'])
+    CSVArray.push(['Numéro article', 'Nom', 'Description', 'Condition', 'Quantité', 'Prix de vente', "Prix d'achat", "Prix total", "Prix total", "Marge/bénéfice", "Marge/bénéfice", 'Image', 'URL'])
     data.forEach((datum) => {
-        CSVArray.push([null, null, datum.title, datum.ref, datum.condition, datum.quantity, datum.price, null, datum.image, datum.url])
+        CSVArray.push([null, null, datum.title, datum.condition, datum.quantity, datum.price, null, null, null, null, null, datum.image, datum.url])
     })
     return CSVArray
 }
